f3c146a7e18093ed6dff2d9ceb0da45b529bd186
dellharshil <harshilribadiya1204@gmail.com>
2024-01-09 23:02:42 +0530

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:choice_collection/resources/color.dart';
+import 'package:choice_collection/widget/text.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const CommonText.bold("Choice Wedding Collection",size: 17,),
+        backgroundColor: AppColor.primary,
+      ),
+
+    );
+  }
+}
